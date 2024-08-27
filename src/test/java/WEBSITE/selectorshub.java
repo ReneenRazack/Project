0223eb365a8434a64804548e5846cff780be398c
ReneenRazack <rene.ninu@gmail.com>
2024-08-27 22:45:26 +0530

    commit

--- src/test/java/WEBSITE/selectorshub.java
+++ src/test/java/WEBSITE/selectorshub.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+
 import org.testng.annotations.Test;
 
 public class selectorshub {
